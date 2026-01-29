6119884020b34c4b6cf1cab83f8cbe2851aee45d
Ether0228 <161998451+Ether0228@users.noreply.github.com>
2026-01-29 10:56:30 +0800

    Update scraper_cloud_feishu.py

--- scraper_cloud_feishu.py
+++ scraper_cloud_feishu.py
@@ -307,8 +307,7 @@ def start_cloud_scraper():
                     "原始通知": raw_text,
                     "提交时间": time_str,
                     "作业链接": {
-                        "text": row['link'],  # <--- 关键：强制让显示的文字等于链接本身
-                        "link": row['link']   # 实际跳转的地址
+                        "link": row['link']   # 飞书也支持这种简写，但 text 默认为空或链接本身
                     },
                     "唯一ID": unique_id
                 }
